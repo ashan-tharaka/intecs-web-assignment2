c24eb13bb8df04b21f560ef99a332599811636c5
ashan-tharaka <ashantharaka715@gmail.com>
2022-09-13 14:26:22 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+function btn1Clicked(){
+    
+        alert("Hotel1 Booked");
+
+ 
+}
+
+function btn2Clicked(){
+    
+    alert("Hotel2 Booked");
+
+
+}
+function btn3Clicked(){
+    
+    alert("Hotel3 Booked");
+
+
+}
+function btn4Clicked(){
+    
+    alert("Hotel4 Booked");
+
+
+}
+function btn5Clicked(){
+    
+    alert("Hotel5 Booked");
+
+
+}
